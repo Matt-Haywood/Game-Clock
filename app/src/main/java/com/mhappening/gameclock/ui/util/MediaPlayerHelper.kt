a6a148da0e5bb07fa187de5c0d5f22c9bab7bd9c
Matt H <133232702+Matthollemue@users.noreply.github.com>
2024-07-22 18:21:53 +0100

    added audioFocusRequest handling to pause other audio when alarm is ringing.

--- app/src/main/java/com/mhappening/gameclock/ui/util/MediaPlayerHelper.kt
+++ app/src/main/java/com/mhappening/gameclock/ui/util/MediaPlayerHelper.kt
@@ -2,6 +2,8 @@ package com.mhappening.gameclock.ui.util
 
 import android.content.Context
 import android.media.AudioAttributes
+import android.media.AudioFocusRequest
+import android.media.AudioManager
 import android.media.MediaPlayer
 import android.provider.Settings
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -12,6 +14,14 @@ class MediaPlayerHelper @Inject constructor(
 ) {
 
     private var mediaPlayer: MediaPlayer? = null
+    private lateinit var audioFocusRequest: AudioFocusRequest
+    private val audioManager = applicationContext.getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
+    private val focusChangeListener = AudioManager.OnAudioFocusChangeListener { focusChange ->
+        when (focusChange) {
+            AudioManager.AUDIOFOCUS_LOSS -> release()
+        }
+    }
 
     fun prepare() {
         mediaPlayer = MediaPlayer().apply {
@@ -25,14 +35,29 @@ class MediaPlayerHelper @Inject constructor(
             setDataSource(applicationContext, Settings.System.DEFAULT_ALARM_ALERT_URI)
             prepare()
         }
+
+        audioFocusRequest = AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN_TRANSIENT)
+            .setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_ALARM)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_SONIFICATION)
+                    .build()
+            )
+            .setOnAudioFocusChangeListener(focusChangeListener)
+            .build()
     }
 
     fun start() {
-        mediaPlayer?.start()
+        val result = audioManager.requestAudioFocus(audioFocusRequest)
+        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            mediaPlayer?.start()
+        }
     }
 
     fun release() {
         mediaPlayer?.release()
         mediaPlayer = null
+        audioManager.abandonAudioFocusRequest(audioFocusRequest)
+
     }
 }
